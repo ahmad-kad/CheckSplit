61de0b1cc5ead65eaa6c6b38bb2fa2a5c0892f8f
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-11 22:45:43 +0800

    Initial Commit

---
+++ CheckSplit/CheckSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitApp.swift
+//  CheckSplit
+//
+//  Created by ahmad kaddoura on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
